8967e2aca263d105b5a5e482cb026bc446ca650e
zorromatahal <mouathsai@yahoo.fr>
2016-05-03 19:20:03 +0100

    Update bobapp2.py

--- appverif/bobapp2.py
+++ appverif/bobapp2.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Flask, request, render_template
 from getdata import *
 app = Flask(__name__)
@@ -11,7 +12,8 @@ def find(txid):
 	#after importing from getdata we pass the txid
 	data = finddata(txid)
 	print data
-	return data
+	return data[:64] + " enregistree le " +datetime.datetime.fromtimestamp(int(data[64:])).strftime('%Y-%m-%d %H:%M:%S')
+
 
 if __name__ == "__main__":
 	app.run('0.0.0.0',5000,debug = True)
