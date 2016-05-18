a7c99695ca7309c8c123b32725987677ec768eab
zorromatahal <mouathsai@yahoo.fr>
2016-05-18 22:25:03 +0100

    Create bobdex.js

---
+++ webapp/static/bobdex.js
@@ -0,0 +1,14 @@
+//get the txid from the button
+$(document).ready(function() {
+$( "#butt" ).click(function() {
+txid=($("#zorro").val());
+
+//$.get('./find/' + txid, function(donne){
+//	$("#result").text("voici votre hash: "+donne);
+
+$.get('./find/' + txid, function(donne){
+	$("#result").text("voici votre hash: "+donne);
+
+});
+});
+});
